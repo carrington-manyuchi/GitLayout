9d87f9d32368ad33f991e216d6d6e6e77d52fc6d
Carrington <Manyuchi>
2024-01-15 16:00:18 +0200

    Custom Button

---
+++ GitLayout/CustomViews/GitButton.swift
@@ -0,0 +1,20 @@
+//
+//  GitButton.swift
+//  GitLayout
+//
+//  Created by DA MAC M1 157 on 2024/01/15.
+//
+
+import UIKit
+
+class GitButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
